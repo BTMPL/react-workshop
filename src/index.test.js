d3c0c8c98f068101cdc6519537566a51e027c2a1
Bartosz Szczecinski <btm@anfo.pl>
2017-11-25 05:27:50 +0100

    Poprawione testy

--- src/index.test.js
+++ src/index.test.js
@@ -1,13 +1,13 @@
-import jsdom from "jsdom";
-import { mount } from "enzyme";
-
-jsdom.env({  
-  html: "<html><body><div id='root'></div></body></html>",
-});
+import React from "react";
+import Enzyme, { mount } from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
 
 import App from "./index.js";
 
 it("komponent App renderuje paragraf", () => {
-  //const wrapper = mount(<App />);
-  //expect(wrapper.find('p')).toHaveLength(1);
+  const wrapper = mount(<App />);
+  console.log(wrapper.text());
+  console.log(wrapper.html())
+  expect(wrapper.find('p').length).toEqual(1)
 })
