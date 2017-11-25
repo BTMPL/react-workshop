2a41456193d9e2cee3b6648cf8ef2a544072b65b
Bartosz Szczecinski <btm@anfo.pl>
2017-11-25 05:10:06 +0100

    03 start

--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,13 @@
-it("na razie nie ma co testowac :)", () => {
-  
+import jsdom from "jsdom";
+import { mount } from "enzyme";
+
+jsdom.env({  
+  html: "<html><body><div id='root'></div></body></html>",
+});
+
+import App from "./index.js";
+
+it("komponent App renderuje paragraf", () => {
+  //const wrapper = mount(<App />);
+  //expect(wrapper.find('p')).toHaveLength(1);
 })
