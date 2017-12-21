396e79136fc2e796638e45db8566335f056ae961
Bartosz Szczecinski <btm@anfo.pl>
2017-12-21 19:25:38 +0100

    Added solution to 07

---
+++ src/solution/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { render } from "react-dom";
+
+const userAvatarUrl = 'https://github.com/btmpl.png';
+const userName = 'Bartosz Szczeciński';
+const time = (new Date()).toString();
+
+export const UserDetails = () => (
+  <div>
+    <img src={userAvatarUrl} width="50" />
+    <b>{userName}</b>  
+  </div>
+)
+
+export const Tweet = () => (
+  <div>
+    <UserDetails />
+    <time>{time}</time>
+  </div>
+)
+
+const App = () => (
+  <div>
+    {
+      [0, 1, 2, 3, 4].map(item => <Tweet key={item} />)
+    } 
+  </div>
+)
+
+render(<App />, document.getElementById("root"));
+
+export default App;
